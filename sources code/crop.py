26ecbb5922d170645893c3e4e6dac1523030b85f
Xi Wang <59131505+xidaniel@users.noreply.github.com>
2020-05-06 20:52:52 -0400

    Add files via upload

---
+++ sources code/crop.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+import os
+
+
+def cropImg(img):
+    gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY_INV)
+    image, contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    con_size = [len(contours[i]) for i in range(len(contours))]
+    index = con_size.index(max(con_size))
+    contours = [contours[index]]
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        #print(x, y, w, h)
+        #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    result = img[y:y+h, x:x+w]
+    #result = cv2.cvtColor(crop, cv2.COLOR_BAYER_BG2BGRA)
+
+    return result
+
+
+def run():
+    ROOT_DIR = os.getcwd()
+    IMAGE_INPUT = ROOT_DIR + '/images/tick/'
+    IMAGE_OUTPUT = ROOT_DIR + '/images/tick_croped/'
+    file_names = next(os.walk(IMAGE_INPUT))[2]
+    size = len(file_names)
+    for i, file_name in enumerate(file_names):
+        if file_name == ".DS_Store":
+            continue
+        img = cv2.imread(os.path.join(IMAGE_INPUT, file_name))
+        if file_name[-4:] == '.tif':
+            namePrefix = file_name.split('.tif')[0]
+        elif file_name[-4:] == '.jpg':
+            namePrefix = file_name.split('.jpg')[0]
+        elif file_name[-4:] == '.png':
+            namePrefix = file_name.split('.png')[0]
+        crop = cropImg(img)
+        cv2.imwrite(IMAGE_OUTPUT + namePrefix + '.png', crop)
+        size -= 1
+        print('Completed {}th image and left {} images'.format(i, size))
+
+
+
---
+++ sources code/inpainting.py
@@ -0,0 +1,30 @@
+import numpy as np
+  import cv2
+
+  # read image and do some damage on image
+  img = cv2.imread('images/3627527276_6fe8cd9bfe_z.jpg')
+  cv2.line(img, (10,70), (200,200), (0,0,255), 2, 4)
+  cv2.line(img, (515,118), (434,372), (0,255,0), 2, 8)
+  cv2.circle(img, (300,300), 5, (255,0,0), 5)
+  #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+  # make mask, mask area must great than recover area
+  mask = np.zeros(img.shape[:2], np.uint8)
+  cv2.line(mask, (10,70), (200,200), (255,255,255), 2, 4)
+  cv2.line(mask, (515,118), (434,372), (255,255,255), 2, 8)
+  cv2.circle(mask, (300,300), 5, (255,255,255), 5)
+
+  # use dilate to extend area of mask
+  kernel = np.ones((3,3), np.uint8)
+  mask = cv2.dilate(mask, kernel, iterations=1)
+
+  # try to use two different kinds of algorithm
+  dst = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
+  dst1 = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
+
+
+  cv2.imshow('original',img)
+  cv2.imshow('dst', dst)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
